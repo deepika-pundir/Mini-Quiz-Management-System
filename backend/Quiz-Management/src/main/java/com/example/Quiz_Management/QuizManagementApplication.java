10d93f3da8ce86f744367218d3ee9727bc9f5fde
Deepika Pundir <dpundir606@gmail.com>
2026-09-22 13:58:54 +0530

    add explicit mongo config v2

--- backend/Quiz-Management/src/main/java/com/example/Quiz_Management/QuizManagementApplication.java
+++ backend/Quiz-Management/src/main/java/com/example/Quiz_Management/QuizManagementApplication.java
@@ -1,16 +1,12 @@
-
 package com.example.Quiz_Management;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class QuizManagementApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(QuizManagementApplication.class, args);
-		System.out.println("=== DEBUG2: Spring resolved spring.data.mongodb.uri as: [" + context.getEnvironment().getProperty("spring.data.mongodb.uri") + "] ===");
+		SpringApplication.run(QuizManagementApplication.class, args);
 	}
 
 }
